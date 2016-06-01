12af13270c3702a525de41cf22d9b79e773190db
Akash Thorat <oflocon502@gmail.com>
2016-06-01 12:34:38 +0530

    O(nlogn) using Priotrity queue

--- Graph/WeightedDijsktra.cpp
+++ Graph/WeightedDijsktra.cpp
@@ -10,15 +10,15 @@ void dijkstra(int v){
 	 pq.push({d[v], v});
 	
 	 while(!pq.empty()){
-			 u = pq.top().second;
+			 u = pq.top().ss;
 			pq.pop();
 			if(mark[u])
 			continue;
 			mark[u] = true;
 			for(auto p : g[u]){ //g[v][i] = pair(vertex, weight)
-				if(d[p.first] > d[u] + p.second){
-					d[p.first] = d[u] + p.second;
-					pq.push({d[p.first], p.first});
+				if(d[p.ff] > d[u] + p.ss){
+					d[p.ff] = d[u] + p.ss;
+					pq.push({d[p.ff], p.ff});
 				}
 			}
 	}
