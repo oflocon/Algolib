9b186327a1202b2cccbd801f869d1a4c5d5cceb7
Akash Thorat <oflocon502@gmail.com>
2016-06-16 15:18:54 +0530

    starter pack

---
+++ template.cpp
@@ -0,0 +1,27 @@
+ /*Let the fight begin....*/
+#include <bits/stdc++.h>
+#define ff first
+#define re return
+#define ss second
+#define pb push_back
+#define mpk make_pair
+#define MOD 1000000007
+#define couts(a) cout<<a<<endl
+#define fr(i,a,b) for(ll i=a;i<b;++i)
+#define rf(i,a,b) for(ll  i=a;i>=b;--i)
+#define coutd(a,b) cout<<a<<" "<<b<<endl
+#define ioS ios_base::sync_with_stdio(0);cin.tie(0);
+#define coutt(a,b,c)cout<<a<<" "<<b<<" "<<c<<endl;
+#define coutar(a,n)  fr(i,0,n)cout<<a[i]<<" ";cout<<endl;
+#define coutq(a,b,c,d)cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+ 
+# define PI 3.141592653589793238462643383279502
+ 
+using namespace std;
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int,int>pi;
+typedef long long int ll;
+typedef vector<int> vi;
+ 
+const int MAXN=2e6+10;
