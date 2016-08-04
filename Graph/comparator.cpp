8be9bd17911459e403b93a56f4dcd265b56481ed
Akash Thorat <oflocon502@gmail.com>
2016-08-04 14:24:51 +0530

    combined comparator

--- Graph/comparator.cpp
+++ Graph/comparator.cpp
@@ -1,5 +1,15 @@
+// for simple pair
 struct compare{
    bool operator()(const pi &a,const pi &b){
        re a.second>b.second;
     }
 };
+
+//for pair of int and pair
+struct compare{
+   bool operator()(const pair<int,pi> &a,const pair<int,pi> &b){
+       if(a.ff==b.ff)re a.ss.ff>b.ss.ff;
+       else re a.ff>b.ff;
+    }
+};
+// gives ascending order 
