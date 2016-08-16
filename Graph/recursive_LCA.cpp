fc9445e94a065a8a99cf202b77633fb71ad8827e
Akash Thorat <oflocon502@gmail.com>
2016-08-16 23:44:19 +0530

    don't use this often instead use the dp one

--- Graph/recursive_LCA.cpp
+++ Graph/recursive_LCA.cpp
@@ -1,21 +1,10 @@
 //<O(n),O(n)>
 
-//preprocess
-void dfs(int v,int p=-1)
+// the other implementation is far better than this in terms of query 
+
+int LCA(int v,int u)
 {
-	if(par+1)h[v]=h[p]+1;
-	par[v]=p;
-
-	fr(i,0,g[v].size()){
-		if(p!=g[v][i])dfs(g[v][i],v);
-	}
-}
-
-//query
-
-int LCA(int v,int u){
 	if(v==u)re v;
-
 	if(h[v]<h[u])swap(u,v);
 
 	re LCA(par[v],u);
@@ -27,5 +16,7 @@ int LCA(int v,int u){
 //<O(n),O(sqrt(n))>
 //sparse table 
 //<O(nlog(n)),O(1)>
+//dp
+//<O(nlogn,logn)>  this is implemented 
 //Advance RMQ
 //<O(n),O(1)> 
