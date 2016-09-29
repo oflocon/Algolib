de3e7a249b3e19eb1a17b8a6bed5794706fd573a
Akash Thorat <oflocon502@gmail.com>
2016-09-29 19:16:04 +0530

    Update Z-algo.cpp

--- String/Z-algo.cpp
+++ String/Z-algo.cpp
@@ -1,20 +1,26 @@
 // very helpful in finding match of a string in other 
 // O(n+m)
 // first concatenate the strings , then build the z array
+//Z[i] is the length of the longest substring starting from S[i] which is also a prefix of S
 
 int L = 0, R = 0;
-for (int i = 1; i < n; i++) {
-  if (i > R) {
-    L = R = i;
-    while (R < n && s[R-L] == s[R]) R++;
-    z[i] = R-L; R--;
-  } else {
-    int k = i-L;
-    if (z[k] < R-i+1) z[i] = z[k];
-    else {
-      L = i;
-      while (R < n && s[R-L] == s[R]) R++;
-      z[i] = R-L; R--;
+for (int i = 1; i < n; i++) 
+{
+    if (i > R)
+    {
+        L = R = i;
+        while (R < n && s[R-L] == s[R]) R++;
+        z[i] = R-L; R--;
+    } 
+    else 
+    {
+        int k = i-L;
+        if (z[k] < R-i+1) z[i] = z[k];
+        else 
+        {
+            L = i;
+            while (R < n && s[R-L] == s[R]) R++;
+            z[i] = R-L; R--;
+        }
     }
-  }
 }
