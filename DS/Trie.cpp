f9c3ce04a5e2ca995c653e22222817c5e5ded84b
Akash Thorat <oflocon502@gmail.com>
2016-09-30 11:55:10 +0530

    Update Trie.cpp

--- DS/Trie.cpp
+++ DS/Trie.cpp
@@ -1,75 +1,30 @@
 
+int trie[27][MAXN],ed[MAXN],vis[MAXN];
+int sz=0;
 
-#define alpsize 26
-
-int geti(char s){re (int)(s-'a');}
-
-
-class Trie
+void add(string s)
 {
-    struct node {
-     node* child[alpsize];
-     int occur;
-    };
-   
-   public:
-    node* root;
-    Trie(){root=new node();root->occur=0;}
-    
-    void insert(string s);
-    void remove(string s);
-    int search(string s);
-};
-  
-void Trie::insert(string s)
-{
-    node* cur=root;
-    cur->occur++;
-    
-    for(int i=0;i<s.length();i++)
-    {
-        int x=geti(s[i]);
-        if(cur->child[x]==NULL)cur->child[x]=new node();
-        cur->child[x]->occur++;
-        cur=cur->child[x];
-    }
+	int v=0;
+	fr(i,0,s.size())
+	{
+		int c=s[i]-'a';
+		if(!vis[trie[c][v]])
+		{
+			trie[c][v]=++sz;
+			vis[trie[c][v]]=1;
+		}
+		v=trie[c][v];
+	}
+	ed[v]++;
 }
-
-void Trie::remove(string s)
+int search(string s)
 {
-    node* cur=root;
-    cur->occur--;
-    node* temp;
-    
-    for(int i=0;i<s.length();i++)
-    {
-        
-        int x=geti(s[i]);
-        temp=cur->child[x];
-        temp->occur--;
-    }
-    
-}
-
-int Trie::search(string s)
-{
-    node* cur=root;
-    
-    for(int i=0;i<s.length();i++)
-    {
-        int x=geti(s[i]);
-        if(cur->child[x]==NULL)re 0;
-        cur=cur->child[x];
-    }
-    re cur->occur;//for prefix count or re 1 for found
-}
-    
-int main()
-{
-    Trie t;
-    t.insert("ddfada");
-    t.insert("ddfda");
-    t.insert("fsad");
-    int p=t.search("dd");
-    couts(p);
+	int v=0;
+	fr(i,0,s.size())
+	{
+		int c=s[i]-'a';
+		if(!vis[trie[c][v]])re 0;
+		v=trie[c][v];
+	}
+	re ed[v]>0;
 }
