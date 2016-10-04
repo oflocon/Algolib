71d61369da8d1fab8cc2460eb70f24d91a551a68
Akash Thorat <oflocon502@gmail.com>
2016-10-04 18:59:39 +0530

    Update primes.cpp

--- NumberTheory/primes.cpp
+++ NumberTheory/primes.cpp
@@ -6,38 +6,38 @@ vector <ll > sol,primes;
 void sieveprecal()
 {
 	ll i,j;
-  for(i=2; i < 100010;i++)
-    {
-	if(arr[i] == 0)               // marks prime numbers using sieve
-        { 
-	    primes.pb(i);
-            isPrim[i]=1;
-            j=i;
-  	    while(j <100010){arr[j] = 1;j+=i;}
+  	for(i=2; i < 100010;i++)
+   	 {
+		if(arr[i] == 0)               // marks prime numbers using sieve
+        	{ 
+	   	 primes.pb(i);
+            	isPrim[i]=1;
+            	j=i;
+  	    	while(j <100010){arr[j] = 1;j+=i;}
 
-	 }
+	 	}
     	
-    }
+    	}
 }
 
 
 void sieveandlcmprefix()             //using sieve for calculation of prefix lcm
 {
-  lcm[1]=1;lcm[0]=0;
-  for(int i=2; i < 300010;i++)
-    {
-	if(!arr[i])
-        {
-            	int j=i;
-		while(j <300010){arr[j] = i;j+=i;}
-        }
-         int  p=arr[i],j=i;
-         while(j%p==0)j/=p;
+  	lcm[1]=1;lcm[0]=0;
+ 	 for(int i=2; i < 300010;i++)
+    	{
+		if(!arr[i])
+        	{
+            		int j=i;
+			while(j <300010){arr[j] = i;j+=i;}
+        	}
+        	 int  p=arr[i],j=i;
+         	while(j%p==0)j/=p;
 
-         if(j==1)lcm[i]=(lcm[i-1]*p)%MOD;
-         else lcm[i]=lcm[i-1];
+        	if(j==1)lcm[i]=(lcm[i-1]*p)%MOD;
+         	else lcm[i]=lcm[i-1];
 
-     }
+     	}
 }
 
 
@@ -67,29 +67,48 @@ void sieve(ll a ,ll b)              // sieve from a to b ,where range of a and b
 
 
 
-int isprime(int x)                // if a single prime is given use this test ,for multiples use above one.//
-{
-    fr(i,2,sqrt(x)+1){if(x%i==0)re 0;}
-     re 1;
-}
-
-
 void phi()                 //phi function 
 {
 
-  int phi[MAXN];
+  	int phi[MAXN];
 
-  fr(i,2,n+1)phi[i]=i;
+  	fr(i,2,n+1)phi[i]=i;
 
-  fr(i,2,n+1)
-  {
-    if(phi[i]==i)
-    {
-        for(int j=i;j<=n;j+=i)phi[j]=(phi[j]/i)*(i-1);
-    }
-  }
+  	fr(i,2,n+1)
+  	{
+    		if(phi[i]==i)
+    		{
+        		for(int j=i;j<=n;j+=i)phi[j]=(phi[j]/i)*(i-1);
+    		}
+  	}
+}
+// calculate mobius for every value upto MAXN,first is for smallest prime fators
+ll mob[MAXN];int p[MAXN];
+
+void sieve()
+{
+	p[1]=1;
+	fr(i,2,MAXN)
+	{
+		if(p[i]==0)
+		{
+			for(int j=i;j<MAXN;j+=i)if(p[j]==0)p[j]=i;
+		}
+	}
 }
 
+void mobius()
+{
+	mob[1]=1;
+	fr(i,2,MAXN)
+	{
+		if(p[i/p[i]]==p[i])mob[i]=0;
+		else
+		{
+			mob[i]=-1*mob[i/p[i]];
+		}
+	}
+}
 
 
 
